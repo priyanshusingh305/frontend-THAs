3baa57ac6587b268b2ce54e936b8997229a3b2c7
Priyanshu Singh <72139258+priyanshusingh305@users.noreply.github.com>
2021-07-01 13:34:47 +0530

    Update index.js

--- DAY8/index.js
+++ DAY8/index.js
@@ -1,8 +1,3 @@
-const squareEl = document.querySelector(".square");
-
-for (let i = 0; i < 400; i++) {
-	squareEl.innerHTML += `<div class = "box"></div>`;
-}
 
 const boxEl = document.querySelectorAll(".box");
 
