a54b6e82f7ac3c2ff2e8bd2d1869f6b329403494
Priyanshu Singh <72139258+priyanshusingh305@users.noreply.github.com>
2021-07-03 12:20:50 +0530

    Add files via upload

---
+++ DAY13/script.js
@@ -0,0 +1,48 @@
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+let input1 = document.querySelector('.input1');
+let h2 = document.querySelector('h2')
+let btn = document.querySelector('button');
+let h1 = document.querySelector('h1');
+let h3 = document.querySelector('h3');
+
+document.querySelector('input').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        if(input1.value!=""){
+        weather();
+        temp();
+        wind();
+    }
+   
+
+
+    }
+});
+btn.addEventListener('click', () => {
+    if(input1.value!=""){
+        weather();
+        temp();
+        wind();
+    }
+})
+function temp(){
+    h1.innerHTML = '';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input1.value}&appid=d41bd08dfe921b7e09a9ab6f38787cf8`)
+
+        .then((res) => res.json())
+        .then((weather) => h2.innerHTML = (Math.round(weather.main.feels_like-273.15)+" °C"))
+}
+function weather(){
+    h1.innerHTML = '';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input1.value}&appid=d41bd08dfe921b7e09a9ab6f38787cf8`)
+
+        .then((res) => res.json())
+        .then((weather) => h1.innerHTML = (weather.weather[0].description))
+}
+function wind(){
+    h1.innerHTML = '';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input1.value}&appid=d41bd08dfe921b7e09a9ab6f38787cf8`)
+
+        .then((res) => res.json())
+        .then((weather) => h3.innerHTML=(Math.round(weather.wind.speed*1.60934)+" km/hr"))
+}
