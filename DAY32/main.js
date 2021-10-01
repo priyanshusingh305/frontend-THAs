594a4f134c9d7e1cf519c317509e666cca1daf80
priyanshusingh305 <singhpriyanshu305@gmail.com>
2021-10-01 17:10:59 +0530

    😉

---
+++ DAY32/main.js
@@ -0,0 +1,77 @@
+"use strict";
+exports.__esModule = true;
+// let a= 10;
+// let b="HEllo"
+// let a;
+// a=10;
+// a="hello";
+/* DATA TYPE
+let n:number =10;
+let s:string = "hello";
+let b:boolean =true;
+let nu:null =null;
+*/
+/*Array
+// normal array
+let a =[1,2,3];
+// number array
+let a :number[]=[1,2,3];
+//string array
+let s :string[]=["hii"];
+// array with both number and string using unison sign
+let a:(number| string)[]=[1,2,5,"sss"];
+a.push("illo");
+// similary
+let a:(number| string | boolean)[]=[1,2,5,"sss"];
+a.push("illo");
+a.push(false);
+// object
+const user ={
+    name :"priyanshu",
+    age: 20
+}
+// object using interface
+interface User {
+    name: string,
+    age:number
+}
+let  user :User ={
+    name: "john",
+    age:20
+}
+
+console.log(user);
+// class based componengt
+class App{
+  printHello(){
+      console.log('hello');
+  }
+}
+const app = new App();
+console.log(app.printHello());
+
+
+
+// two data type assignment
+let username : string|null ="";
+console.log(username);
+
+
+
+// custom data type
+type Door = "open" | "close";
+let  door:Door="open"; //only open and closed can be stored as per custom data type
+
+// any data type
+let a:any;
+a=10;
+a="hello"
+a=true
+
+
+// fuction with return datatype number
+function add(a:number,b:number ):number{
+   return a+b;
+}
+console.log(add (1,5));
+*/ 
